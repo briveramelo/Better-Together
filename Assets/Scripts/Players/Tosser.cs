a2dc5c85e2325f5ff25b1d0257c388f21bf13345
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2015-10-24 12:47:41 -0600

    NEW EFFECTS!
    Got the realistic effects packs baby

--- Assets/Scripts/Players/Tosser.cs
+++ Assets/Scripts/Players/Tosser.cs
@@ -6,12 +6,24 @@ public class Tosser : MonoBehaviour {
 
 	public Controls controls;
 	private Collider[] collidersToToss;
-	[Range(200,10000)]
 	private float tossForce;
-	public Transform targetTossTransform; 
+	private float yFactor; 
+
+//	struct BackPack{
+//		public static BackPack empty = new BackPack(0,0);
+//
+//		float weight;
+//		int numNoteBooks;
+//		public BackPack(float weight, int numNoteBooks){
+//			this.weight = weight;
+//			this.numNoteBooks = numNoteBooks;
+//		}
+//
+//	}
 
 	void Awake(){
 		tossForce = 600f;
+		yFactor = 7.5f;
 	}
 
 	void OnTriggerStay(Collider col){
@@ -23,8 +35,7 @@ public class Tosser : MonoBehaviour {
 	}
 
 	void Toss(Collider col){
-		Vector3 tossDir = (targetTossTransform.position-col.transform.position).normalized;
-		float forceMultiplier = Mathf.Clamp01 (Vector3.Distance(targetTossTransform.position,col.transform.position));
-		col.GetComponent<Rigidbody>().AddForce(tossDir * tossForce * forceMultiplier);
+		Vector3 tossDir = new Vector3 (-Mathf.Sign(controls.transform.localScale.x),yFactor,0f).normalized;
+		col.GetComponent<Rigidbody>().AddForce(tossDir * tossForce);
 	}
 }
